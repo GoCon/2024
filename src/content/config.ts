c8581c2dcda63d9a58462bfe5e10027a7e25be1a
sago35 <sago35@gmail.com>
2024-02-25 10:41:23 +0900

    Staff 一覧ページを作成

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,23 @@ const sponsorsCollection = defineCollection({
     }),
   ),
 });
+
+const staffCollection = defineCollection({
+  type: "data",
+  schema: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      icon: z.string(),
+      organizationName: z.string(),
+      twitterLink: z.string(),
+      githubLink: z.string(),
+      otherLink: z.string(),
+    }),
+  ),
+});
+
 export const collections = {
   sponsors: sponsorsCollection,
+  staff: staffCollection,
 };
