4b9b4152b5ae4b2097e8fd008f40cb162971bd4b
budougumi0617 <budougumi0617@gmail.com>
2024-03-16 11:01:02 +0900

    docs: add function comment

--- src/utils/makePath.ts
+++ src/utils/makePath.ts
@@ -1,3 +1,6 @@
+// パスを組み立てるときに環境変数を使ってルート情報を追加する関数。
+// GitHub Pages で公開する都合で astro.config.ts で base を指定している。
+// Netlifyで公開するときはbaseを上書きして公開しているので動的に指定する必要がある。
 export function makePath(path?: string | null): string {
   if (!path) {
     return import.meta.env.BASE_URL;
