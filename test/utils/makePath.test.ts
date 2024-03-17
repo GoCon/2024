e86075c8b3f486f6b339bf069410266ebe8422d1
budougumi0617 <budougumi0617@gmail.com>
2024-03-17 09:17:54 +0900

    refactor: 変数名を修正 && 引数の型を修正

--- test/utils/makePath.test.ts
+++ test/utils/makePath.test.ts
@@ -1,20 +1,16 @@
-import { makePath } from "../../src/utils/makePath";
+import { concatWithBase } from "../../src/utils/concatWithBase";
 import { describe, assert, expect, test } from "vitest";
 
-describe("makePath", () => {
+describe("concatWithBase", () => {
   test("prefixにBASE_URLを追加する", () => {
-    expect(makePath("path")).eq(`${import.meta.env.BASE_URL}/path`);
+    expect(concatWithBase("path")).eq(`${import.meta.env.BASE_URL}/path`);
   });
 
   test("空文字が渡されたらBASE_URLだけを返す", () => {
-    expect(makePath("")).eq(`${import.meta.env.BASE_URL}`);
-  });
-
-  test("NULLが渡されたらBASE_URLだけを返す", () => {
-    expect(makePath(null)).eq(`${import.meta.env.BASE_URL}`);
+    expect(concatWithBase("")).eq(`${import.meta.env.BASE_URL}`);
   });
 
   test("引数がなかったらBASE_URLだけを返す", () => {
-    expect(makePath()).eq(`${import.meta.env.BASE_URL}`);
+    expect(concatWithBase()).eq(`${import.meta.env.BASE_URL}`);
   });
 });
